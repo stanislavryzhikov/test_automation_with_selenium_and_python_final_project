98ef85c645c1c79c00bc80c24d73e19ccb978c1a
Stanislav Ryzhikov <stanislavryzhikov@gmail.com>
2020-03-27 22:23:08 +0300

    Task: content independence, looking for a bug

--- test_product_page_2.py
+++ test_product_page_2.py
@@ -1,10 +1,21 @@
+import pytest
+
 from .pages.product_page import ProductPage
 import time
 
 # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-
-def test_guest_can_add_product_to_basket(browser):
+# link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
     # Открываем страницу товара
     # (http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear). Обратите внимание,
     # что в ссылке есть параметр "?promo=newYear". Не теряйте его в авто-тесте, чтобы получить проверочный код.
@@ -22,4 +33,4 @@ def test_guest_can_add_product_to_basket(browser):
     product_page.solve_quiz_and_get_code()
     product_page.should_be_added_to_cart()
     product_page.should_be_match_price()
-# pytest -s -v --tb=line --language=en test_product_page.py
+# pytest -s -v --tb=line --language=en test_product_page_2.py
